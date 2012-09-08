fc560a5c1724abe12277867e265dc65d9a112acc
Erik Michaels-Ober <sferik@gmail.com>
2012-09-07 17:19:17 -0700

    Major changes for Twitter API v1.1

--- lib/twitter/response/parse_json.rb
+++ lib/twitter/response/parse_json.rb
@@ -9,10 +9,6 @@ module Twitter
         case body
         when /\A^\s*$\z/, nil
           nil
-        when 'true'
-          true
-        when 'false'
-          false
         else
           MultiJson.load(body, :symbolize_keys => true)
         end
@@ -20,7 +16,7 @@ module Twitter
 
       def on_complete(env)
         if respond_to?(:parse)
-          env[:body] = parse(env[:body]) unless [204, 302, 304].include?(env[:status])
+          env[:body] = parse(env[:body]) unless [204, 301, 302, 304].include?(env[:status])
         end
       end
 
